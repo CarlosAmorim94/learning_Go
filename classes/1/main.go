84368ff1435501f4bfb9548987e9e14872ec6e5b
carlos <carlosa@xgrow.com>
2023-10-04 14:42:52 -0300

    More types of variables

--- classes/1/main.go
+++ classes/1/main.go
@@ -1,5 +1,19 @@
 package main
 
+const a = "Hello World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "teste"
+	e float64 = 12.3
+)
+
 func main() {
-	println("Hello World!")
+	a := "x"
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
 }
