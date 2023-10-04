5104c3f43ae6f9368fad789409611d5240f91cdd
carlos <carlosa@xgrow.com>
2023-10-04 14:45:23 -0300

    Create type

--- classes/1/main.go
+++ classes/1/main.go
@@ -2,11 +2,14 @@ package main
 
 const a = "Hello World!"
 
+type ID int
+
 var (
 	b bool    = true
 	c int     = 10
-	d string  = "teste"
+	d string  = "Carlos"
 	e float64 = 12.3
+	f ID      = 123456
 )
 
 func main() {
@@ -16,4 +19,5 @@ func main() {
 	println(c)
 	println(d)
 	println(e)
+	println(f)
 }
