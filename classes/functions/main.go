4ea27344e20d670c755f427b2fdcb5e8634bc904
carlos <carlosa@xgrow.com>
2023-10-04 20:56:10 -0300

    Learning functions

--- classes/functions/main.go
+++ classes/functions/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	value, error := sum(9, 2)
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	fmt.Println(value)
+
+}
+
+func sum(a, b int) (int, error) {
+	if a+b > 10 {
+		return 0, errors.New("Valor maior que 10")
+	}
+	return a + b, nil
 }
